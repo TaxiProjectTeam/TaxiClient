9b9b685dc7e48529d104d501a90a298dc26da9c8
Vlad <vlads0n>
2017-01-25 22:13:18 +0200

    Added geolocation

--- app/src/main/java/com/example/sveta/taxo/MainActivity.java
+++ app/src/main/java/com/example/sveta/taxo/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.sveta.taxo;
 
-import android.*;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,9 +23,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DatabaseReference;
+
+import java.io.IOException;
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -33,27 +36,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public static final String ORDERS_CHILD = "orders";
     private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 13;
-    private EditText startingStreet;
-    private EditText startingHouse;
-    private EditText startingEntrance;
-    private EditText destinationStreet;
-    private EditText destinationHouse;
-    private DatabaseReference databaseReference;
     private GoogleMap googleMap;
     private GoogleApiClient googleApiClient;
     private Location lastLocation;
     private LatLng marker;
     private MarkerOptions markerOptions;
+    private boolean mapReady = false;
+
+    private EditText startingAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
         if (googleApiClient == null) {
             googleApiClient = new GoogleApiClient.Builder(this)
@@ -63,14 +59,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .build();
         }
 
+        startingAddress = (EditText) findViewById(R.id.starting_address);
+
         ImageView getLocation = (ImageView) findViewById(R.id.get_my_location);
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mapReady) {
+                    marker = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                    googleMap.addMarker(markerOptions.position(marker).title("Ви знаходитесь тут"));
 
+                    CameraPosition camera = CameraPosition.builder().target(marker).zoom(17).build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
+
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+                    List<Address> addresses = null;
+                    try {
+                        addresses = geocoder.getFromLocation(lastLocation.getLatitude(), lastLocation.getLongitude(), 1);
+                    } catch (IOException e) {}
+                    String city = addresses.get(0).getLocality();
+                    String street = addresses.get(0).getAddressLine(0);
+                    startingAddress.setText(city + ", " + street);
+                }
             }
         });
-
 
         Button buttonOrder = (Button) findViewById(R.id.order);
         buttonOrder.setOnClickListener(new View.OnClickListener() {
@@ -80,12 +92,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 startActivity(intent);
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     protected void onStart() {
-        googleApiClient.connect();
         super.onStart();
+        googleApiClient.connect();
     }
 
     @Override
@@ -96,11 +112,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        mapReady = true;
         this.googleMap = googleMap;
-        marker = new LatLng(49.441153, 32.064271);
         markerOptions = new MarkerOptions();
-        this.googleMap.addMarker(markerOptions.position(marker).title("Marker in Cherkassy"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
         this.googleMap.getUiSettings().setZoomControlsEnabled(true);
 
         this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -109,6 +123,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 googleMap.addMarker(markerOptions.position(latLng));
             }
         });
+
     }
 
     @Override

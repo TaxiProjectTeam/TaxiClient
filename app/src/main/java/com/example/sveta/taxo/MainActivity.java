ac21a32686f12b31f653dc8ba98506563a45031d
Vlad <vlads0n>
2017-01-25 19:27:00 +0200

    Added map

--- app/src/main/java/com/example/sveta/taxo/MainActivity.java
+++ app/src/main/java/com/example/sveta/taxo/MainActivity.java
@@ -1,28 +1,23 @@
 package com.example.sveta.taxo;
 
+import android.*;
 import android.Manifest;
-import android.app.Activity;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,9 +27,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DatabaseReference;
 
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String ORDERS_CHILD = "orders";
+    private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 13;
     private EditText startingStreet;
     private EditText startingHouse;
     private EditText startingEntrance;
@@ -42,20 +40,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private EditText destinationHouse;
     private DatabaseReference databaseReference;
     private GoogleMap googleMap;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+    private LatLng marker;
+    private MarkerOptions markerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
@@ -66,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
             }
         });
 
@@ -82,24 +84,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     protected void onStart() {
-        mGoogleApiClient.connect();
+        googleApiClient.connect();
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        mGoogleApiClient.disconnect();
+        googleApiClient.disconnect();
         super.onStop();
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         this.googleMap = googleMap;
-
-        LatLng cherkassy = new LatLng(49.441153, 32.064271);
-        final MarkerOptions markerOptions = new MarkerOptions();
-        this.googleMap.addMarker(markerOptions.position(cherkassy).title("Marker in Cherkassy"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(cherkassy));
+        marker = new LatLng(49.441153, 32.064271);
+        markerOptions = new MarkerOptions();
+        this.googleMap.addMarker(markerOptions.position(marker).title("Marker in Cherkassy"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
         this.googleMap.getUiSettings().setZoomControlsEnabled(true);
 
         this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -112,7 +113,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_FINE_LOCATION);
+        }
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
     }
 
     @Override
@@ -123,5 +127,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }

277ae503784a767d8207d7abaca660cd9c9712be
vlad_son <19vlad_son90@bk.ru>
2017-03-29 18:48:50 +0300

    Change order address

---
+++ app/src/main/java/com/example/sveta/taxo/activity/AddressBlankActivity.java
@@ -0,0 +1,15 @@
+package com.example.sveta.taxo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sveta.taxo.R;
+
+public class AddressBlankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_blank);
+    }
+}
